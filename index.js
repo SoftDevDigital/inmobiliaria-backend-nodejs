7263a6ec0a21d21b44fe54b786bd3588d574379a
AlexAc0226 <alexis.correa026@gmail.com>
2025-11-03 11:58:38 -0300

    Enhance CORS configuration to restrict origins and specify allowed methods and headers

--- index.js
+++ index.js
@@ -8,7 +8,22 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middlewares
-app.use(cors());
+const allowedOrigins = ['http://localhost:3000', 'https://intercanjes.com'];
+const corsOptions = {
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      return callback(null, true);
+    }
+    return callback(new Error('Not allowed by CORS'));
+  },
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
